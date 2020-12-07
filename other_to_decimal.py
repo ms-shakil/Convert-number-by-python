f5103eb52782d36b62905f908fee4c0c6c433edc
Sahabuddin Shakil <mdshakil8796@gmail.com>
2020-12-07 09:28:03 +0600

    change some code in other to decimal file

--- other_to_decimal.py
+++ other_to_decimal.py
@@ -3,17 +3,17 @@ def other_to_decimal(lst,convert):
      lst2 =[]
      for i in lst:
           j =0
-          if i =="A":
+          if i =="A" or i =="a":
                i =10
-          elif i =="B":
+          elif i =="B" or i =="b":
                i =11
-          elif i == "C":
+          elif i == "C" or i=="c":
                i = 12
-          elif i == "D":
+          elif i == "D" or i =="d":
                i=13
-          elif i == "E":
+          elif i == "E" or i =="e":
                i = 14 
-          elif i =="F":
+          elif i =="F" or i=="f":
                i =15
           j= int(i)
           lst2.append(j)
